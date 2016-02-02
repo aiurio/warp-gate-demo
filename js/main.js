68cd5595633e3f2e64bdc2357699c8f9667167d7
Andre Kradolfer <narfdre@gmail.com>
2016-02-02 09:33:38 -0700

    adding facebook + stripe ui

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('.step_form .btn').on('click', finishStep);
 
-$('#fbLogin').on('click', fbLoginClick);
+$('#fbLogin').on('click', login);
 
 //Functions
 function finishStep(e){
@@ -11,25 +11,51 @@ function finishStep(e){
   if(next) $('#step_' + next).addClass('active');
 }
 
-
-function fbLoginClick(){
-	console.log('starting');
-	window.onbeforeunload = confirmExit;
-    function confirmExit() {
-        return "You have attempted to leave this page. Are you sure?";
+function login(){
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me',{fields: 'first_name,last_name,email,picture'}, function(response) {
+          console.log(response);
+          $('.userStart').hide();
+          $('.userForm').show();
+          $('#firstName').attr('value', response.first_name);
+          $('#lastName').attr('value', response.last_name);
+          $('#email').attr('value', response.email);
+          $('#user_avatar').attr('src', response.picture.data.url);
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    }else{
+      console.log(response);
     }
-	FB.login(function(response) {
-		window.onbeforeunload = null;
-		console.log(response);
-		alert('response');
-		
-	    if (response.authResponse) {
-		    console.log('Welcome!  Fetching your information.... ');
-		    FB.api('/me', function(response) {
-		    	console.log('Good to see you, ' + response.name + '.');
-		    });
-	    } else {
-	    	console.log('User cancelled login or did not fully authorize.');
-	    }
-	});	
+  });
 }
+
+var handler = StripeCheckout.configure({
+    key: 'pk_T20xIUBLEJGrjK8dhW9Yit8iq45I7',
+    image: '/img/documentation/checkout/marketplace.png',
+    locale: 'auto',
+    token: function(token) {
+      console.log(token);
+      $('[data-step="two"]').click();
+      // Use the token to create the charge with a server-side script.
+      // You can access the token ID with `token.id`
+    }
+  });
+
+  $('#payStripe').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Adella',
+      description: 'Membership',
+      amount: 4999
+    });
+  });
+
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
+  });
