a1fbc1189633a81686c8af6dd330be0b3de660f8
Andre Kradolfer <narfdre@gmail.com>
2016-02-05 22:50:31 -0700

    adding server stuff... use npm start

---
+++ client/js/main.js
@@ -0,0 +1,68 @@
+$('.step_form .btn').on('click', finishStep);
+$('#fbLogin').on('click', login);
+
+var customer = {};
+//Functions
+var actions = {
+  saveCustomerInfo: function(){
+    customer.first_name = $('#firstName').val();
+    customer.last_name = $('#lastName').val();
+    customer.email = $('#email').val();
+    customer.name = customer.first_name + customer.last_name;
+    console.log(customer);
+  },
+  processPayment: function(){
+    window.runPayment(customer,
+                      $('#cc_number').val(),
+                      $('#cvc_number').val(),
+                      $('#exp_month').val(),
+                      $('#exp_year').val());
+  }
+};
+
+function finishStep(e){
+  var step = $(e.target).data('step');
+  var next = $(e.target).data('next');
+  var run  = $(e.target).data('run');
+  $('.step[data-step=' + step + ']').addClass('done');
+  $('#step_' + step).removeClass('active');
+  if(next) $('#step_' + next).addClass('active');
+  if(run) actions[run]();
+}
+
+function login(){
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me',{fields: 'first_name,last_name,email,picture'}, function(response) {
+          console.log(response);
+          $('.userStart').hide();
+          $('.userForm').show();
+          $('#firstName').attr('value', response.first_name);
+          $('#lastName').attr('value', response.last_name);
+          $('#email').attr('value', response.email);
+          $('#user_avatar').attr('src', response.picture.data.url);
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    }else{
+      FB.login(function(){
+        login();
+      }, {scope: 'public_profile,email'});
+      console.log(response);
+    }
+  });
+}
+
+$(window).scroll(function(e){
+  $('#h2').css('transform', 'translate3d(0px, ' + $(window).scrollTop() * 0.6 + 'px, 0px)');
+  if((window.pageYOffset +  window.innerHeight + 200) >  $('#brushes').offset().top){
+    $('#brushes').css('opacity', '1');
+  }
+});
+
+if((window.pageYOffset +  window.innerHeight + 200) >  $('#brushes').offset().top){
+  $('#brushes').css('opacity', '1');
+}
---
+++ server/index.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var Stripe = require("stripe");
+
+var app = express();
+var port = 8000;
+app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.post('/api/customers/', onCustomerPost);
+
+app.listen(port, function(){
+  console.log('CORS-enabled web server listening on port', port);
+});
+
+// Example Payload for creating a customer (AND CHARING)
+/*
+// HEADERS
+{
+	"x-stripe-key": "STRIPE_SECRET_KEY_HERE",
+	"x-stripe-token": "STRIPE_TOKEN_FROM_STRIPE.JS_LIB"
+}
+// BODY
+{
+  "amount": 100,
+  "customer": {
+    "name": "Andre Kradolfer"
+  },
+  "description": "Test test",
+  "metadata": {
+    "foo": "bar"
+  }
+}
+*/
+
+
+
+function onCustomerPost(req, res, next){
+
+	var key = req.headers['x-stripe-key'];
+	// the newly created customer's token
+	var token = req.headers['x-stripe-token'];
+
+	console.log('Attempting connection with stripe key', key);
+	console.log('Using token ', token);
+
+
+
+	var stripe = Stripe(key);
+	stripe.customers.create({
+		description: req.body.customer.name,
+    email: req.body.customer.email,
+		source: token,
+	}, onCustomer);
+
+	function onError(err, msg){
+		console.warn('Failed:', err);
+		res.json({
+			msg: msg,
+			error: err
+		});
+	}
+
+	function onComplete(){
+	  	res.send({
+	  		msg: 'This is CORS-enabled for all origins!',
+	  		body: req.body,
+	  		headers: req.headers
+		});
+	}
+
+	function onChargeComplete(chargeErr, charge, customer) {
+		if(chargeErr){
+			return onError(chargeErr, 'We created a customer, but failed to charge them...');
+		}
+
+		onComplete(customer, charge);
+	}
+
+	function onCustomer(custErr, customer){
+		if( custErr ){
+			return onError(custErr, 'There was an error creating the customer');
+		}
+
+		console.log('Created customer', customer);
+
+		stripe.charges.create({
+			currency: "usd",
+			customer: customer.id,
+			amount: req.body.amount,
+			metadata: req.body.metadata,
+			description: req.body.description
+		}, function(chargeErr, charge){
+			onChargeComplete(chargeErr, charge, customer);
+		});
+	}
+}
